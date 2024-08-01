5a8bc1f348b37b832083effe65184a2863158108
Omar Zayed <omarzayed7777@gmail.com>
2024-08-01 11:21:03 +0400

    Added running and hitting with sword sounds

--- Player/States/RunPlayerState.gd
+++ Player/States/RunPlayerState.gd
@@ -2,6 +2,8 @@ class_name RunPlayerState
 
 extends State
 
+func enter():
+	PlayerManager.player.get_node("Audio/MoveAudio").playing = true
 
 func update(delta):
 	PlayerManager.player.velocity.x = PlayerManager.player.direction.x * PlayerManager.player.SPEED
@@ -10,8 +12,11 @@ func update(delta):
 		PlayerManager.player.get_node("Rogue_Hooded").rotation.y = lerp_angle(PlayerManager.player.get_node("Rogue_Hooded").rotation.y, atan2(PlayerManager.player.direction.x, PlayerManager.player.direction.z), delta * 10) 
 	PlayerManager.player.get_node("AnimationTree")["parameters/playback"].travel("Running_A")
 	if not PlayerManager.player.direction and PlayerManager.player.is_on_floor():
+		PlayerManager.player.get_node("Audio/MoveAudio").playing = false
 		transition.emit("IdlePlayerState")
 	elif PlayerManager.player.is_on_floor() and Input.is_action_just_pressed("Jump"):
+		PlayerManager.player.get_node("Audio/MoveAudio").playing = false		
 		transition.emit("JumpPlayerState")
 	elif Input.is_action_just_pressed("Attack") and PlayerManager.player.is_on_floor():
+		PlayerManager.player.get_node("Audio/MoveAudio").playing = false
 		transition.emit("SwordSwingPlayerState")
