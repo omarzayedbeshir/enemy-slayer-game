91e2bac2dff9e50c685698548963dc0f7da70d7d
Omar Zayed <omarzayed7777@gmail.com>
2024-08-07 22:32:44 +0400

    Added enemy scene, moving and hitting states

---
+++ Scripts/enemy.gd
@@ -0,0 +1,58 @@
+extends CharacterBody3D
+
+var see_player = false
+var attack_player = false
+var SPEED = 2.0
+var health = 12
+var direction
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var mesh = $Barbarian
+@onready var anim = $Barbarian/AnimationPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	var player = PlayerManager.player
+	var direction = (player.position - position).normalized()
+	if see_player:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		anim.play("Running_A")
+		mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(direction.x, direction.z), delta * 10)
+	elif attack_player:
+		velocity.x = 0
+		velocity.z = 0
+		anim.play("1H_Melee_Attack_Slice_Diagonal")
+		mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(direction.x, direction.z), delta * 10)		
+	else:
+		velocity.x = 0
+		velocity.z = 0
+		anim.play("Idle")		
+	move_and_slide()
+
+
+func _on_chase_player_body_entered(body):
+	if body.name == "Player":
+		see_player = true
+
+
+func _on_chase_player_body_exited(body):
+	if body.name == "Player":
+		see_player = false
+
+
+func _on_attack_player_body_entered(body):
+	if body.name == "Player":
+		see_player = false
+		attack_player = true	
+
+
+func _on_attack_player_body_exited(body):
+	if body.name == "Player":
+		attack_player = false
+		see_player = true
