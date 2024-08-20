3c4fb8159616b97e80ddb8947f9d70bf3378cf29
Omar Zayed <omarzayed7777@gmail.com>
2024-08-20 21:11:27 +0400

    Added ability to use items from hotbar using numbers

--- Scripts/level_1.gd
+++ Scripts/level_1.gd
@@ -3,8 +3,11 @@ extends Node3D
 @onready var player = $Player
 @onready var inventory_interface = $UI/InventoryInterface
 const PickUp = preload("res://item/pick_up.tscn")
+@onready var hot_bar_inventory = $UI/HotBarInventory
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	hot_bar_inventory.set_inventory_data(player.inventory_data)
 	inventory_interface.set_player_inventory_data(player.inventory_data)
 	player.toggle_inventory.connect(toggle_inventory_interface)
 	inventory_interface.force_close.connect(toggle_inventory_interface)
@@ -14,8 +17,10 @@ func _ready():
 func toggle_inventory_interface(external_inventory_owner = null):
 	inventory_interface.visible = not inventory_interface.visible
 	if inventory_interface.visible:
+		hot_bar_inventory.hide()
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	else:
+		hot_bar_inventory.show()
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	if external_inventory_owner and inventory_interface.visible:
 		inventory_interface.set_external_inventory(external_inventory_owner)
---
+++ inventory/hot_bar_inventory.gd
@@ -0,0 +1,29 @@
+extends PanelContainer
+
+@onready var h_box_container = $MarginContainer/HBoxContainer
+const slot = preload("res://inventory/slot.tscn")
+signal hot_bar_use(index)
+
+func _unhandled_key_input(event):
+	if not visible or event.is_pressed():
+		return
+	
+	if range(KEY_1, KEY_7).has(event.keycode):
+		hot_bar_use.emit(event.keycode - KEY_1)
+		
+func set_inventory_data(inventory_data):
+	hot_bar_use.connect(inventory_data.use_slot_data)
+	inventory_data.inventory_updated.connect(populate_hot_bar)
+	populate_hot_bar(inventory_data)
+
+func populate_hot_bar(inventory_data: InventoryData):
+	for child in h_box_container.get_children():
+		child.queue_free()
+	
+	for slot_data in inventory_data.slot_datas.slice(0, 6):
+		var slot = slot.instantiate()
+		h_box_container.add_child(slot)
+		
+		
+		if slot_data:
+			slot.set_slot_data(slot_data)
