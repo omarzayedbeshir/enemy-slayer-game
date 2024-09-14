126ed39f17906ed7f4c199f520a8a800f0014ac7
Omar Zayed <omarzayed7777@gmail.com>
2024-09-14 12:36:16 +0400

    final final final touches

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -2,14 +2,12 @@ extends CanvasLayer
 
 
 func _unhandled_key_input(_event):
-	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().paused = not get_tree().paused
-	
-	if get_tree().paused and Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("ui_cancel") and not get_tree().paused:
 		show()
+		get_tree().paused = true
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	elif not $"../UI/InventoryInterface".visible:
-		hide()                                                  
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	else:
+	elif Input.is_action_just_pressed("ui_cancel") and get_tree().paused:
+		if not $"../UI/InventoryInterface".visible:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		hide()
+		get_tree().paused = false
